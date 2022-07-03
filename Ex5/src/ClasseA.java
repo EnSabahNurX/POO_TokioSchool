f3a80427218225b4a6482df8b0aa79e47332eb86
EnSabahNurX <ricklimadealmeida@gmail.com>
2022-07-03 23:03:06 +0100

    todas as classes inclusive a main feitas

--- Ex5/src/ClasseA.java
+++ Ex5/src/ClasseA.java
@@ -2,8 +2,27 @@ public class ClasseA {
     // Desenvolver 3 classes (A, B e C) definindo em cada uma delas, pelo menos, 3 tipos de variáveis com 3
     // modificadores de acesso diferentes.
 
-    protected int idade;
-    public String nome;
-    private float altura;
 
+    private final float altura;
+    public String nome;
+
+    protected int idade;
+
+    public ClasseA(String nome, int idade, float altura) {
+        this.idade = idade;
+        this.nome = nome;
+        this.altura = altura;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
 }
