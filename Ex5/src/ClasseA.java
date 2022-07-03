f218ac492126265ffd1c11a92df9fd6408d3ba2b
EnSabahNurX <ricklimadealmeida@gmail.com>
2022-07-03 21:46:34 +0100

    classe A pronta

---
+++ Ex5/src/ClasseA.java
@@ -0,0 +1,9 @@
+public class ClasseA {
+    // Desenvolver 3 classes (A, B e C) definindo em cada uma delas, pelo menos, 3 tipos de variáveis com 3
+    // modificadores de acesso diferentes.
+
+    protected int idade;
+    public String nome;
+    private float altura;
+
+}
